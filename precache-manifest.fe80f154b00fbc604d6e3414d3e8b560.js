746a10f5906429511527682c8801b7909ac4648b
tosicky <tosinadelegan@yahoo.com>
2020-07-05 23:09:02 -0300

    Updates

---
+++ precache-manifest.fe80f154b00fbc604d6e3414d3e8b560.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadfa0514e5cef054e2d39ef833db17d",
+    "url": "/TA-resume/index.html"
+  },
+  {
+    "revision": "1829f95bd89496568d26",
+    "url": "/TA-resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "769c107645221a3deae8",
+    "url": "/TA-resume/static/js/2.cdb1efeb.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/TA-resume/static/js/2.cdb1efeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1829f95bd89496568d26",
+    "url": "/TA-resume/static/js/main.e563f907.chunk.js"
+  },
+  {
+    "revision": "1aa90e3960b0c1258b18",
+    "url": "/TA-resume/static/js/runtime-main.e2317b1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TA-resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ba858177d960edb0930dde11219cf7b",
+    "url": "/TA-resume/static/media/profile-pic.5ba85817.jpg"
+  }
+]);
